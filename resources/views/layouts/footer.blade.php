efa8bf1f1b06235428138b17ae2271f0c1013d0e
hyungjuun <hyungjuun@gmail.com>
2021-07-09 19:14:14 +0900

    v0 footer 블레이드 추가함.

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="container-fluid">
+    <div class="row">
+        <div class="col-sm-12 col-xs-12 nwfooter hidden-xs ">
+            <span>회사명 (서비스 회사명)  : 주관사 ㈜골드스타 333 | 주소 : 강원도 화천군 사내면 수피령로 10번지</span>
+            <span class="col-xs-12">관리자 명 : 김대준 | 전화번호 : 00-0000-0000 | Email  : goldstar333@naver.com</span>
+        </div>
+        <div class="col-sm-12 col-xs-12 nwfooter visible-xs ">
+            <span class="col-xs-12">회사명 (서비스 회사명)  : 주관사 ㈜골드스타 333</span>
+            <span class="col-xs-12">주소 : 강원도 화천군 사내면 수피령로 10번지</span>
+            <span class="col-xs-12">관리자 명 : 김대준</span>
+            <span class="col-xs-12">전화번호 : 00-0000-0000</span>
+            <span class="col-xs-12">Email  : goldstar333@naver.com</span>
+        </div>
+    </div>
+</footer>
